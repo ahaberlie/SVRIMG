afdec041c7882e20ee2b8d690bc6ea5759e18777
Alex Haberlie <alex.haberlie@gmail.com>
2021-08-17 17:32:10 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_namespace_packages
 
 setup(name='svrimg',
-      version='0.1a1',
+      version='0.1a2',
       description='SVRIMG Dataset Interface and Analyses',
       author='Alex Haberlie',
       author_email='svr.image@gmail.com',
